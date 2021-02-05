f398b6aea68a6b9dba9d9297ba33526baa648229
nikolinadapic <ndjapi00@fesb.hr>
2021-02-05 20:01:53 +0100

    Added contact data component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -97,41 +97,18 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         //alert('You continue!');
-        /*this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Nikolina Dapic',
-                address: {
-                    street: 'My Street 1',
-                    zipCode: '12345',
-                    country: 'Croatia'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i])); //property_name = property_value (this is an array!)
         }
-        axios.post('/orders.json', order) //.json je radi Firebasea koji koristim
-            .then(response => {
-                this.setState({loading: false, purchasing: false});
-                //console.log(response)
-            })
-            .catch(error => {
-                this.setState({loading: false, purchasing: false});
-                //console.log(error)
-            });*/
-            const queryParams = [];
-            for (let i in this.state.ingredients) {
-                queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i])); //property_name = property_value (this is an array!)
-            }
+        queryParams.push('price=' + this.state.totalPrice);
 
-            const queryString = queryParams.join('&');
+        const queryString = queryParams.join('&');
 
-            this.props.history.push({
-                pathname: '/checkout',
-                search: '?' + queryString //dodajemo to u URL
-            });
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString //dodajemo to u URL
+        });
     }
 
     render() {
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
     state = {
-        ingredients: {
-            salad: 1,
-            meat:1,
-            cheese: 1,
-            bacon: 1
-        }
+        ingredients: null,
+        totalPrice: 0
     }
 
-    componentDidMount() {
+    componentWillMount() {
         const query = new URLSearchParams(this.props.location.search);
         const ingredients = {};
+        let price = 0;
         for (let param of query.entries()) {
             //each entry has this format: ['salad': '1']
-            ingredients[param[0]] = +param[1] //+ is there to convert it to a number
+            if (param[0] === 'price') {
+                price = param[1];
+            } else {
+                ingredients[param[0]] = +param[1] //+ is there to convert it to a number
+            }
         }
-        this.setState({ingredients: ingredients});
+        this.setState({ingredients: ingredients, totalPrice: price});
     }
 
     checkoutCancelledHandler = () => {
@@ -35,6 +38,8 @@ class Checkout extends Component {
                 <CheckoutSummary ingredients={this.state.ingredients}
                     checkoutCancelled={this.checkoutCancelledHandler}
                     checkoutContinued={this.checkoutContinuedHandler} />
+                    <Route path={this.props.match.path + '/contact-data'}
+                        render={(props) => (<ContactData ingredients={this.state.ingredients} totalPrice={this.state.totalPrice} {...props} />)} />
             </div>
         );
     }
