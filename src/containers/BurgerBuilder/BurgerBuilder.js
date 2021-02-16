57ab642aa28a836f0fc45edd118b2232ba3bfcb3
nikolinadapic <ndjapi00@fesb.hr>
2021-02-16 14:43:09 +0100

    Fetching ingredients asynchronously from Firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -17,20 +17,12 @@ class BurgerBuilder extends Component {
         this.state = {}
     }*/
     state = {
-        purchasing: false,
-        loading: false,
-        error: false
+        purchasing: false
     }
 
     componentDidMount() {
         console.log(this.props);
-        /*axios.get('https://react-burger-builder-65b06-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json')
-            .then(response => {
-                this.setState({ingredients: response.data});
-            })
-            .catch(error => {
-                this.setState({error: true});
-            });*/
+        this.props.onInitIngredients();
     }
 
     updatePurchaseState(ingredients) {
@@ -67,7 +59,7 @@ class BurgerBuilder extends Component {
         }
 
         let orderSummary = null;
-        let burger = this.state.error ? <p>Ingredients can't be loaded!</p> : <Spinner />;
+        let burger = this.props.error ? <p>Ingredients can't be loaded!</p> : <Spinner />;
         if (this.props.ings) {
             burger = (
                 <Auxiliary>
@@ -86,9 +78,6 @@ class BurgerBuilder extends Component {
                     purchaseContinued={this.purchaseContinueHandler}
                     price={this.props.price} />
         }
-        if (this.state.loading) {
-            orderSummary = <Spinner />;
-        }
 
         return(
             <Auxiliary>
@@ -104,14 +93,16 @@ class BurgerBuilder extends Component {
 const mapStateToProps = state => {
     return {
         ings: state.ingredients,
-        price: state.totalPrice
+        price: state.totalPrice,
+        error: state.error
     };
 }
 
 const mapDispatchToProps = dispatch => {
     return {
         onIngredientAdded: (ingName) => dispatch(burgerBuilderActions.addIngredient(ingName)),
-        onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName))
+        onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName)),
+        onInitIngredients: () => dispatch(burgerBuilderActions.initIngredients())
     }
 }
 
--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -1,5 +1,6 @@
 //will hold action creators for building a burger (sync actions for adding and removing ingredients)
 import * as actionTypes from './actionTypes';
+import axios from '../../axios-orders';
 
 export const addIngredient = (name) => {
     return {
@@ -12,5 +13,30 @@ export const removeIngredient = (name) => {
     return {
         type: actionTypes.REMOVE_INGREDIENT,
         ingredientName: name
+    };
+};
+
+export const setIngredients = (ingredients) => { //this is the action we want to dispatch once the async code in initIngredients is done
+    return {
+        type: actionTypes.SET_INGREDIENTS,
+        ingredients: ingredients
+    };
+};
+
+export const fetchIngredientsFailed = () => {
+    return {
+        type: actionTypes.FETCH_INGREDIENTS_FAILED
+    };
+};
+
+export const initIngredients = () => {
+    return dispatch => {
+        axios.get('https://react-burger-builder-65b06-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json')
+            .then(response => {
+                dispatch(setIngredients(response.data));
+            })
+            .catch(error => {
+                dispatch(fetchIngredientsFailed());
+            });
     };
 };
